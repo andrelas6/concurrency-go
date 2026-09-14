920728926f629987cc7fcba33e59db20b6196f14
andrelas1 <andre@bide1.com>
2026-09-14 19:38:02 +0200

    concurrent 3 calls still not working

--- channels.go
+++ channels.go
@@ -13,56 +13,81 @@ func main() {
 	// three tasks at a time
 	tasks := getTasks()
 
-	TaskScheduler(ctx, tasks, MAX_CONCURRENT)
+	result := TaskScheduler(ctx, tasks, MAX_CONCURRENT)
+
+	for _, r := range result {
+		fmt.Printf("RESULTS: %d %v\n", r.Id, r.Result)
+	}
 }
 
 func TaskScheduler(ctx context.Context, tasks []Task, maxConcurrent int) (taskResults []TaskResult) {
 	taskCh := make(chan Task, len(tasks))
-	defer close(taskCh)
 	for _, t := range tasks {
 		taskCh <- t
 	}
+	close(taskCh)
 	// out channel - returns the result unbuffered
 	taskResultCh := make(chan TaskResult, len(tasks))
 	defer close(taskResultCh)
-	tokenBudgetCh := make(chan struct{}, maxConcurrent)
-	defer close(tokenBudgetCh)
-	var i int
+	out := make(chan TaskResult, len(tasks))
 
-	// task runner
+	for _ = range maxConcurrent {
+		go func() {
+			task := <-taskCh
+			taskResultCh <- task()
+		}()
+	}
+
 	go func() {
 		for {
 			select {
 			case <-ctx.Done():
 				return
-			case task := <-taskCh:
-				<-tokenBudgetCh
-				i++
-				fmt.Println("RECEIVED NEW TOKEN", i)
-				taskResultCh <- task()
-				tokenBudgetCh <- struct{}{}
+			case result, ok := <-taskResultCh:
+				if !ok {
+					return
+				} else {
+					out <- result
+					select {
+					case task, ok := <-taskCh:
+						if !ok {
+							return
+						} else {
+							go func() {
+								taskResultCh <- task()
+							}()
+						}
+					case <-ctx.Done():
+						return
+					}
+				}
+
 			}
 		}
 	}()
 
-	// read values
-	for result := range taskResultCh {
-		taskResults = append(taskResults, result)
+	for o := range out {
+		taskResults = append(taskResults, o)
 	}
-
-	return taskResults
+	return
 }
 
 // true -> success
 // false -> failure
-type TaskResult bool
+type TaskResult struct {
+	Id     int
+	Result bool
+}
 type Task func() TaskResult
 
 func getTasks() (tasks []Task) {
 	for i := range 10 {
 		task := func() TaskResult {
-			fmt.Printf("Running task %d ...", i)
-			return i%2 == 0
+			fmt.Printf("Running task %d ...\n", i)
+			return TaskResult{
+				Result: i%2 == 0,
+				Id:     i,
+			}
 		}
 		tasks = append(tasks, task)
 	}
